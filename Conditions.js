bdedcd0709efe3d0371f7f066de3a68e21d7f98b
Jakob Slechta <94152918+Jakob-Slechta@users.noreply.github.com>
2021-11-16 16:07:37 -0700

    Add files via upload

---
+++ Conditions.js
@@ -0,0 +1,56 @@
+var firstname = "Jakob";
+
+
+if(firstname == "Jakob"){
+console.log("Jakob it's great to have you back.")
+
+} else {
+    console.log("Hey, " + firstname)
+}
+
+
+var costofDanny = 3.52;
+
+if (costofDanny < 3) {
+    console.log("I'll buy my friend.");
+} else if (costofDanny < 2) {
+    console.log("I will not take the cheap friend.");
+} else {
+    console.log("I'll pass, he's not worth it.");
+}
+
+
+
+var grade = "C";
+
+switch (grade) {
+    case "A":
+        console.log("90-100");
+        break;
+    case "B":
+        console.log("80-89");
+        break;
+    case "C":
+        console.log("70-79");
+        break;
+    default:
+        console.log("0-69");
+}
+
+var a = 75;
+var b = 98;
+
+if (a == 75) {
+    if (b == 98) {
+        console.log("Ninety Eight Point OOOONNNEEEEEEE!")
+    }
+}
+
+
+var a = 75;
+var b = 98;
+
+if (a == 75 && b == 98){
+    console.log("Ninety Eight Point OOOONNNEEEEEEE!")
+}
+
